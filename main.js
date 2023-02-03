d859d7a1460df7d0c4b6a94d6f9201bc5465327e
the spider <euryanspike.repositorio@gmail.com>
2023-02-03 01:28:39 -0300

    Melhorias feitas

--- main.js
+++ main.js
@@ -1,41 +1,69 @@
 const botao = document.querySelector('.enviar')
+const popUp = document.querySelector('.pop-up')
+const close = document.getElementById('close')
+
 let materias = document.querySelector('#materias')
 let pontos = document.querySelector('#pontos')
-const form = document.querySelector('.section')
+
+const popUpMedia = document.querySelector('.popUp_media')
+const popUpDesc = document.querySelector('.popUp_descricao')
 
 
 //Extraidas para uma função
-function alertCalc(calc, msg) {
-    alert(`${'Média de: '}${+calc.toFixed(0)} ${msg}`)
-}
 
 function fix(element) {
-    return element.toFixed(0);
+    return element.toFixed(1);
 }
+
+function popUpScreen (calc,msg) {
+
+    popUpMedia.textContent = `Sua média é: ${fix(calc)}`;
+    popUpDesc.textContent = msg;
+    return;
+}
+
 
 //Calculando a média
 
+
 botao.addEventListener('click', (e) => {
     e.preventDefault();
+
     let calc = parseInt(pontos.value / materias.value);
 
-    if (fix(calc) <= 0) {
+    if (fix(calc) >= 0) {
         const low = 'Sua nota está na baixa, vamos melhorar! :D'
-        alertCalc(calc, low);
-    } else if (fix(calc) <= 3) {
+        popUpScreen(calc, low);
+    } else if (fix(calc) >= 3) {
         const medium = 'Sua nota está na média, vamos melhorar! :D'
-        alertCalc(calc, medium);
-    } else if (fix(calc) >= 8) {
+        popUpScreen(calc, medium);
+    } else if (fix(calc) >= 7) {
         const high = 'Sua nota está na alta, parabéns! :D'
-        alertCalc(calc, high);
+        popUpScreen(calc, high);
+
     }
 
     //Limpando inputs
     materias.value = '';
     pontos.value = '';
 
+    popUp.style.display ='block'
+        
 
 })
+
+//Close Pop-Up
+close.addEventListener('click', () => {
+    popUp.style.display ='none'
+
+})
+
+
+
+
+
+    
+
 
 
 
